c2b86ff3265d8772305f5efa09caa561505c3b48
Mackayyya <1430625575@qq.com>
2026-07-24 00:03:18 +0800

    Add files via upload

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // 将 Supabase 项目中的 Project URL 和 anon public key 填入这里。
 window.READING_ATLAS_SUPABASE = {
-  url: '',
-  anonKey: ''
+  url: 'https://idsdpehzhshfsvzhdpom.supabase.co',
+  anonKey: 'sb_publishable_uluOJqtw_IXYx1U2344n5w_o5ISRZYZ'
 };
